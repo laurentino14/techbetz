1f9c5b09587e6a9b1b6bed12150983a47abf4dab
Lucas Laurentino <laurentinodeev@gmail.com>
2025-01-16 21:00:53 -0300

    Initial Commit

---
+++ TechBeatz/TechBeatzApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechBeatzApp.swift
+//  TechBeatz
+//
+//  Created by Lucas Laurentino on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TechBeatzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
